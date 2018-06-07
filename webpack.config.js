6b1ae9bcd660a336d5e4527a135c108769b58912
Marcus Weiner <marcus.weiner@gmail.com>
2018-06-07 13:34:59 +0200

    :wrench: Use specific babel instructions from upstream

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ const base = {
         rules: [{
             test: /\.jsx?$/,
             loader: 'babel-loader',
-            include: [path.resolve(__dirname, 'src'), /node_modules[\\/]scratch-[^\\/]+[\\/]src/],
+            include: [path.resolve(__dirname, 'src'), /node_modules[\\/](@wdr-data[\\/])?scratch-[^\\/]+[\\/]src/],
             options: {
                 // Explicitly disable babelrc so we don't catch various config
                 // in much lower dependencies.
