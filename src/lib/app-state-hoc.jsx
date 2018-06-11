024e897ab882d53fb1e7bffa2a2bdfa380248d6a
Marcus Weiner <marcus.weiner@gmail.com>
2018-06-11 17:21:41 +0200

    :art: Use global routing in redux

--- src/lib/app-state-hoc.jsx
+++ src/lib/app-state-hoc.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Provider} from 'react-redux';
-import {createStore, combineReducers, compose} from 'redux';
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
+import {initializeCurrentLocation} from 'redux-little-router';
 
 import {intlShape} from 'react-intl';
 import {IntlProvider, updateIntl} from 'react-intl-redux';
 import intlReducer from '../reducers/intl.js';
 
 import guiReducer, {guiInitialState, guiMiddleware, initFullScreen, initPlayer} from '../reducers/gui';
+import * as router from './routing';
 
 import {setPlayer, setFullScreen} from '../reducers/mode.js';
 
 import {ScratchPaintReducer} from 'scratch-paint';
 
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const enhancer = composeEnhancers(guiMiddleware);
+const enhancer = composeEnhancers(router.enhancer, guiMiddleware, applyMiddleware(router.middleware));
 
 /*
  * Higher Order Component to provide redux state. If an `intl` prop is provided
@@ -36,13 +39,19 @@ const AppStateHOC = function (WrappedComponent) {
             const reducer = combineReducers({
                 intl: intlReducer,
                 scratchGui: guiReducer,
-                scratchPaint: ScratchPaintReducer
+                scratchPaint: ScratchPaintReducer,
+                router: router.reducer,
             });
 
             this.store = createStore(
                 reducer,
                 {scratchGui: initializedGui},
                 enhancer);
+
+            const initialLocation = this.store.getState().router;
+            if (initialLocation) {
+                this.store.dispatch(initializeCurrentLocation(initialLocation));
+            }
         }
         componentDidUpdate (prevProps) {
             if (prevProps.intl !== this.props.intl) {
---
+++ src/lib/routing.js
@@ -0,0 +1,24 @@
+import { routerForBrowser } from 'redux-little-router';
+
+export const Views = {
+  start: "VIEW_START",
+  edu: "VIEW_EDU",
+  project: "VIEW_PROJECT",
+}
+
+export const eduUrl = eduId => `/lernspiel/${eduId}`;
+export const projectUrl = projectId => `/projekt/${projectId}`;
+
+const routes = {
+  '/': {
+    view: Views.start,
+  },
+  '/lernspiel/:eduId': {
+    view: Views.edu,
+  },
+  '/projekt/:projectId': {
+    view: Views.project,
+  },
+};
+
+export const { reducer, middleware, enhancer } = routerForBrowser({ routes });
