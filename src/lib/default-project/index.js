62ccd545bd2186c73d1619ef6c2ffe2a0fe339ab
p3nny <p3nny@hacking.studio>
2018-06-12 21:13:06 +0200

    :bento: Adjust default project

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -3,10 +3,8 @@ import projectJson from './project.json';
 
 /* eslint-disable import/no-unresolved */
 import popWav from '!arraybuffer-loader!./83a9787d4cb6f3b7632b4ddfebf74367.wav';
-import meowWav from '!arraybuffer-loader!./83c36d806dc92327b9e7049a565c6bff.wav';
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
-import costume1 from '!raw-loader!./09dc888b0b7df19f70d81588ae73420e.svg';
-import costume2 from '!raw-loader!./3696356a03a8d938318876a593572843.svg';
+import costume1 from '!raw-loader!./83ba8783100ee3c109dad5aac8a71f1d.svg';
 /* eslint-enable import/no-unresolved */
 
 const encoder = new TextEncoder();
@@ -21,23 +19,13 @@ export default [{
     dataFormat: 'WAV',
     data: new Uint8Array(popWav)
 }, {
-    id: '83c36d806dc92327b9e7049a565c6bff',
-    assetType: 'Sound',
-    dataFormat: 'WAV',
-    data: new Uint8Array(meowWav)
-}, {
     id: 'cd21514d0531fdffb22204e0ec5ed84a',
     assetType: 'ImageVector',
     dataFormat: 'SVG',
     data: encoder.encode(backdrop)
 }, {
-    id: '09dc888b0b7df19f70d81588ae73420e',
+    id: '83ba8783100ee3c109dad5aac8a71f1d',
     assetType: 'ImageVector',
     dataFormat: 'SVG',
     data: encoder.encode(costume1)
-}, {
-    id: '3696356a03a8d938318876a593572843',
-    assetType: 'ImageVector',
-    dataFormat: 'SVG',
-    data: encoder.encode(costume2)
 }];
