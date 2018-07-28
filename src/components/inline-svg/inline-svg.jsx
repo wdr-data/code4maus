756455ac9738cb1c24f99f8778c1fffe93a71619
Marcus Weiner <marcus.weiner@gmail.com>
2018-07-28 22:58:37 +0200

    :art: Ensure inline svg sizing via css

--- src/components/inline-svg/inline-svg.jsx
+++ src/components/inline-svg/inline-svg.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import styles from './inline-svg.css';
 
 const InlineSVG = (props) => {
     const {
         svg,
+        className,
         ...componentProps
     } = props;
     return (
-        <span dangerouslySetInnerHTML={{ __html: svg }} {...componentProps} />
+        <span
+            dangerouslySetInnerHTML={{ __html: svg }}
+            className={classNames(styles.wrapper, className)}
+            {...componentProps}
+        />
     );
 };
 
 InlineSVG.propTypes = {
     svg: PropTypes.string.isRequired,
+    className: PropTypes.string,
+};
+
+InlineSVG.defaultProps = {
+    className: '',
 };
 
 export default InlineSVG;
