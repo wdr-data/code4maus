8f243fce32aab91c64aa31c93557be29729db357
Marcus Weiner <marcus.weiner@gmail.com>
2018-07-28 19:22:58 +0200

    :recycle: Move porject listing into component

---
+++ src/components/menu-listing/menu-listing.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Box from '../box/box.jsx';
+
+import styles from './menu-listing.css';
+
+const MenuListingComponent = (props) =>
+    Object.entries(props.projects).map(([ key, project ]) => (
+        <Box
+            key={key}
+            className={styles.projectWrapper}
+            onClick={props.handleProjectClickCreate(key)}
+        >
+            <span>{project.name}</span>
+            <span className={styles.projectChanged}>
+                {new Date(project.updated_at).toLocaleDateString()}
+            </span>
+        </Box>
+    ))
+;
+
+const SHAPE_PROJECT = {
+    name: PropTypes.string.isRequired,
+    created_at: PropTypes.number.isRequired,
+    updated_at: PropTypes.number.isRequired,
+};
+
+MenuListingComponent.propTypes = {
+    projects: PropTypes.objectOf(PropTypes.shape(SHAPE_PROJECT)),
+    handleProjectClickCreate: PropTypes.func.isRequired,
+};
+
+export default MenuListingComponent;
