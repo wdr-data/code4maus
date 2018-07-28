75306301ef8ac8adfdc0d5955bf8868f96589538
Marcus Weiner <marcus.weiner@gmail.com>
2018-07-28 19:22:31 +0200

    :art: Fix code style in menu

--- src/containers/menu.jsx
+++ src/containers/menu.jsx
@@ -1,39 +1,44 @@
 import React from 'react';
-import {Views} from '../lib/routing';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 import MenuComponent from '../components/menu/menu.jsx';
-import {connect} from 'react-redux';
 
 const bucketUrl = process.env.S3_BUCKET_URL_PROJECT;
 
 class Menu extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
 
         this.state = {
-            projects: []
-        }
+            projects: {},
+        };
     }
-    componentDidMount () {
+    componentDidMount() {
         this.loadUserProjects();
     }
-    async loadUserProjects () {
+    async loadUserProjects() {
         const projects = await (await fetch(`${bucketUrl}/projects/${this.props.userId}/index.json`)).json();
         this.setState({ projects });
     }
-    render () {
+    render() {
         return (
             <MenuComponent projects={this.state.projects} />
-        )
+        );
     }
 }
 
-const mapStateToProps = state => ({
+Menu.propTypes = {
+    userId: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
     router: {
         view: state.router.result.view,
     },
     userId: state.scratchGui.project.userId,
 });
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = (dispatch) => ({});
 
 export default connect(
     mapStateToProps,
