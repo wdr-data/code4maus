3780c1adc8beb72d46cffc22c857f032434bfac6
Marcus Weiner <marcus.weiner@gmail.com>
2018-07-30 15:31:10 +0200

    :wrench: Hide edu game 0

--- src/lib/edu/index.js
+++ src/lib/edu/index.js
@@ -1,10 +1,5 @@
 export default [
     {
-        id: '00',
-        name: 'Lernspiel 0',
-        subtitle: 'Lerne die Oberfläche kennen!',
-    },
-    {
         id: '01',
         name: 'Lernspiel 1',
         subtitle: 'Dein erstes Spiel',
