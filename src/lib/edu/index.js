a6e05b18a74fcf9491413f9e6b933dd803d4eaba
p3nny <ennenbach@worthandlung.de>
2018-09-03 22:31:44 +0200

    :sparkles: Add Lernspiel 02

--- src/lib/edu/index.js
+++ src/lib/edu/index.js
@@ -1,17 +1,17 @@
 export default [
     {
         id: '01',
-        name: '01 Trickfilm',
-        subtitle: 'Die Maus blinzelt',
+        name: 'Die Maus blinzelt',
+        subtitle: '01 Trickfilm',
     },
     {
         id: '02',
-        name: '02 Schleifen',
-        subtitle: 'Tanz, Maus tanz',
+        name: 'Maus-Tanz',
+        subtitle: '02 Schleifen',
     },
     {
         id: '03',
-        name: '03 Mehr Figuren',
-        subtitle: 'Wimmelbild',
+        name: 'Wimmelbild',
+        subtitle: '03 Mehr Figuren',
     },
 ];
