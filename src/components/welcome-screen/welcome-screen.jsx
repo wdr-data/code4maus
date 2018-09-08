13feef24ddad191f06fa03e626d3796371d9c542
Christine <christine.gotth@gmail.com>
2018-09-08 16:13:31 +0200

    :lipstick: Add background to welcome screen

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -9,13 +9,15 @@ import styles from './welcome-screen.css';
 const WelcomeScreenComponent = (props) => (
     <LogoOverlay>
         <div className={styles.wrapper}>
-            <p>Willkommen zu <strong>Programmieren mit der Maus.</strong></p>
-            <p>
-                Die Welt wird digitaler. Programmieren ist in dieser Welt eine Grundfertigkeit,
-                um selbst aktiv werden, mitgestalten und auch Kritik üben zu können. Mit dieser
-                Anwendung programmieren Kinder Schritt für Schritt erste eigene Bildergeschichten
-                und kleine Spiele. Dabei lernen sie Variablen, Schleifen und Verzweigungen kennen.
-            </p>
+            <div className={styles.innerWrapper}>
+                <p>Willkommen zu <strong>Programmieren mit der Maus.</strong></p>
+                <p>
+                    Die Welt wird digitaler. Programmieren ist in dieser Welt eine Grundfertigkeit,
+                    um selbst aktiv werden, mitgestalten und auch Kritik üben zu können. Mit dieser
+                    Anwendung programmieren Kinder Schritt für Schritt erste eigene Bildergeschichten
+                    und kleine Spiele. Dabei lernen sie Variablen, Schleifen und Verzweigungen kennen.
+                </p>
+            </div>
             <div className={styles.buttonWrapper}>
                 <ButtonPrimary className={styles.button} onClick={props.onIntroClick}>
                     Zur Einführung
