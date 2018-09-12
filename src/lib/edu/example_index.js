0f362baff8067f45cda0788a8e4ac93475c67b6f
p3nny <p3nny@hacking.studio>
2018-09-12 10:13:08 +0200

    :bento: Add and organise examples in edu

---
+++ src/lib/edu/example_index.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        id: 'example01',
+        name: 'Kühlschrank-Memo',
+        subtitle: 'Jannes Höke',
+    },
+    {
+        id: 'example02',
+        name: 'Füttere den Elefanten',
+        subtitle: 'Wilfried Elmenreich',
+    },
+    {
+        id: 'example03',
+        name: 'Hilfe! Die Ente ist weg',
+        subtitle: 'Gretchen Elmenreich',
+    },    
+    {
+        id: 'example04',
+        name: 'Weltraum-Abenteuer',
+        subtitle: 'Johanna Janiszewski',
+    },
+    
+];
