316e68d96ffce21c318049dd099073a797a28a17
Christine <christine.gotth@gmail.com>
2018-09-13 10:09:13 +0200

    :art: Add proper reference for thumbnail images

--- src/lib/edu/index.js
+++ src/lib/edu/index.js
@@ -1,38 +1,45 @@
+import image00 from '../edu/00/assets/000.png'
+import image01 from '../edu/01/assets/000.png'
+import image02 from '../edu/02/assets/000.png'
+import image03 from '../edu/03/assets/000.png'
+import image04 from '../edu/04/assets/000.png'
+import image05 from '../edu/05/assets/000.png'
+
 export default [
     {
         id: '00',
         name: 'Hallo Welt',
         subtitle: '00 Erste Schritte',
-        image: '../edu/00/assets/000.png',
+        image: image00,
     },
     {
         id: '01',
         name: 'Die Weltraum-Maus',
         subtitle: '01 Bewegung',
-        image: '../edu/01/assets/000.png',
+        image: image01,
     },
     {
         id: '02',
         name: 'Die Maus blinzelt',
         subtitle: '02 Trickfilm',
-        image: '../edu/02/assets/000.png',
+        image: image02,
     },
     {
         id: '03',
         name: 'Die Maus tanzt!',
         subtitle: '03 Schleifen',
-        image: '../edu/03/assets/000.png',
+        image: image03,
     },
     {
         id: '04',
         name: 'Wimmelbild',
         subtitle: '04 Mehr Figuren',
-        image: '../edu/04/assets/000.png',
+        image: image04,
     },
     {
         id: '05',
         name: 'Kugelfisch',
         subtitle: '01 Bewegung',
-        image: '../edu/05/assets/000.png',
+        image: image05,
     },
 ];
