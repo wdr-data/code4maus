0c0005321e4d610a4496d04688597521da778657
Marcus Weiner <marcus.weiner@gmail.com>
2018-09-15 16:22:37 +0200

    :rocket: Use hashes to invalidate caches

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: '[name].js',
+        filename: '[name].[contenthash].js',
         publicPath: '/',
     },
     module: {
