70f3e6721ab4f3499989730246a41e0cdd221845
Marcus Weiner <marcus.weiner@gmail.com>
2018-09-15 15:43:25 +0200

    :rocket: Rewrite frontend deployment with aws cli

---
+++ scripts/deploy-frontend.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# todo: make dynamic via serverless
+BUCKET="hackingstudio-code4maus-app"
+
+if [ "$BRANCH" == "production" ]; then
+    BUCKET="${BUCKET}-prod"
+else
+    BUCKET="${BUCKET}-staging"
+fi
+
+aws s3 sync build s3://${BUCKET} --delete --acl public-read --dryrun
