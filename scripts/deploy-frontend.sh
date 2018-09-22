8b6d6e121acb003f58b0373b46e0aa2296bffb3e
Marcus Weiner <marcus.weiner@gmail.com>
2018-09-22 23:47:59 +0200

    :bug: Disable caching for index.html

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -10,3 +10,4 @@ else
 fi
 
 aws s3 sync build s3://${BUCKET} --delete --acl public-read
+aws s3 cp s3://${BUCKET}/index.html s3://${BUCKET}/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
