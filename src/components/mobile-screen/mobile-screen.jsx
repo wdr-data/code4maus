1cf430f8f60692a1d66d4d21f9bd089ac7894d8e
Marcus Weiner <marcus.weiner@gmail.com>
2018-11-29 18:29:00 +0100

    :sparkles: Link from mobile overlay to parent info

--- src/components/mobile-screen/mobile-screen.jsx
+++ src/components/mobile-screen/mobile-screen.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import Box from '../box/box.jsx';
+import { Link } from 'redux-little-router';
 
+import Box from '../box/box.jsx';
 import InlineSVG from '../inline-svg/inline-svg.jsx';
+import Button from '../button-primary/button-primary.jsx';
 
 import mausQuestion from '!raw-loader!../../../assets/img/maus_question.svg';
 
@@ -14,6 +16,11 @@ const MobileScreenComponent = () => (
                 Leider kannst Du <strong>Programmieren mit der Maus</strong> nicht auf dem Smartphone nutzen.
                 Im Moment werden nur Tablets und Desktop-Rechner unterstützt.
             </p>
+            <Link href="/inhalte/eltern">
+                <Button className={styles.infoButton}>
+                    Mehr Infos
+                </Button>
+            </Link>
         </div>
         <InlineSVG className={styles.mausQuestion} svg={mausQuestion} />
     </Box>
