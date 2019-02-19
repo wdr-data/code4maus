c9105df959fd27773e23c24c4ee5bbd2f8bfbb04
Christine <christine.gotth@gmail.com>
2019-02-19 17:52:27 +0100

    :memo: Add storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,14 @@
+const mainConfig = require('../webpack.config');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    module: mainConfig.module,
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: 'static',
+                to: 'static',
+            }
+        ]),
+    ],
+}
