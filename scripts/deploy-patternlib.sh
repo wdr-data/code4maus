8e0896260c2ca2e4f3a6c78ce9457cb891433567
Marcus Weiner <marcus.weiner@gmail.com>
2019-02-20 12:35:35 +0100

    :ambulance: Fix gh-pages deploy via ssh

--- scripts/deploy-patternlib.sh
+++ scripts/deploy-patternlib.sh
@@ -10,11 +10,8 @@ yarn build-storybook -o .patternlib
 export AUTHOR_EMAIL="$(git log -1 $TRAVIS_COMMIT --pretty="%aE")"
 export AUTHOR_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
 
-echo "$GITHUB_DEPLOY_KEY" > /tmp/ssh-deploy-pages
-chmod 600 /tmp/ssh-deploy-pages
-
 eval "$(ssh-agent -s)"
-ssh-add /tmp/ssh-deploy-pages
+echo "$GITHUB_DEPLOY_KEY" | ssh-add -
 
 yarn gh-pages -d .patternlib \
               -m "Update patternlib from ${TRAVIS_BRANCH}" \
