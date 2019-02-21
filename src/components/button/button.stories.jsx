a2f53b2f264e3234327b5a26d167aa73a7a1c538
Christine <christine.gotth@gmail.com>
2019-02-21 12:28:47 +0100

    :memo: Add to button pattern

--- src/components/button/button.stories.jsx
+++ src/components/button/button.stories.jsx
@@ -6,6 +6,15 @@ storiesOf('Button', module)
     .add('default', () => (
         <Button />
     ))
+    .add('primary', () => (
+        <Button primary='true'></Button>
+    ))
+    .add('secondary', () => (
+        <Button secondary='true'></Button>
+    ))
+    .add('wiggle', () => (
+        <Button wiggle='true' primary='true'></Button>
+    ))
     .add('disabled', () => (
         <Button disabled='true'>
         </Button>
