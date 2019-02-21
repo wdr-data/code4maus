a02b15a4aafbdb6689a01c267e18535e3888ed83
p3nny <p3nny@hacking.studio>
2019-02-21 18:01:45 +0100

    :bento: Add re:publica flower mouse example

--- src/lib/edu/examples.js
+++ src/lib/edu/examples.js
@@ -6,6 +6,7 @@ import image06 from './beispiel06/assets/000.png';
 import image07 from './beispiel07/assets/000.png';
 import image08 from './beispiel08/assets/000.png';
 import image09 from './beispiel09/assets/000.png';
+import image10 from './beispiel10/assets/000.png';
 
 export default [
     {
@@ -56,4 +57,10 @@ export default [
         subtitle: 'Die Maus findet deine Zahl',
         image: image09,
     },
+    {
+        id: 'beispiel10',
+        name: '#MausArt',
+        subtitle: 'Lass Blumen sprechen',
+        image: image10,
+    }
 ];
