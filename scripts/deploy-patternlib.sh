6e7d901a0d962b24852f73a2d0a5467cb0d96400
Marcus Weiner <marcus.weiner@gmail.com>
2019-02-21 12:29:28 +0100

    :rocket: Deploy Storybook to GitHub Pages

--- scripts/deploy-patternlib.sh
+++ scripts/deploy-patternlib.sh
@@ -10,8 +10,10 @@ yarn build-storybook -o .patternlib
 export AUTHOR_EMAIL="$(git log -1 $TRAVIS_COMMIT --pretty="%aE")"
 export AUTHOR_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
 
+echo "${GITHUB_DEPLOY_KEY}" | base64 --decode > /tmp/deploy_key
+chmod 600 /tmp/deploy_key
 eval "$(ssh-agent -s)"
-echo "$GITHUB_DEPLOY_KEY" | ssh-add -
+ssh-add /tmp/deploy_key
 
 yarn gh-pages -d .patternlib \
               -m "Update patternlib from ${TRAVIS_BRANCH}" \
