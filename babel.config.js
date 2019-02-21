329a423d75a02ea23227fc61376bbae440bb0487
Marcus Weiner <marcus.weiner@gmail.com>
2019-02-21 16:22:29 +0100

    :arrow_up: Upgrade babel config to v7 style

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable array-bracket-newline */
+/* eslint-disable array-bracket-spacing */
+module.exports = {
+    plugins: [
+        '@babel/syntax-dynamic-import',
+        '@babel/transform-async-to-generator',
+        ['react-intl', {
+            'messagesDir': './translations/messages/',
+        }],
+    ],
+    'presets': [
+        ['@babel/env',
+            { 'targets': { 'browsers': [ 'last 3 versions', 'Safari >= 8', 'iOS >= 8' ] } },
+        ],
+        '@babel/react',
+    ],
+};
