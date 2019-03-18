a9013f63b7eef792b30bb92259c4009c8f95f730
Andreas Streichardt <andreas@mop.koeln>
2019-03-18 15:52:53 +0100

    ✨ Offline support

---
+++ plugins/generate-s3-sw-precache-plugin/index.js
@@ -0,0 +1,37 @@
+const crypto = require('crypto');
+const { getAllAssets } = require('../../scripts/lib/assets');
+
+function GenerateS3SWPrecachePlugin(options) {
+    this.filename = options.filename || 's3-manifest.[hash].js';
+}
+
+GenerateS3SWPrecachePlugin.prototype.apply = function(compiler) {
+    let filenameTemplate = this.filename;
+    compiler.plugin('emit', function(compilation, callback) {
+        getAllAssets()
+            .then((files) => {
+                const list = JSON.stringify(files.map((file) => '/data/assets/' + file));
+                const hash = crypto.createHash('md5').update(list, 'utf8').digest('hex');
+                const filename = filenameTemplate.replace('[hash]', hash);
+                const absUrl = '/' + filename;
+
+                compilation.assets[filename] = {
+                    source: function() {
+                        return `self.__precacheManifest = (self.__precacheManifest || []).concat(${list});`;
+                    },
+                    size: function() {
+                        return this.source().length;
+                    },
+                };
+
+                const swSource = compilation.assets['service-worker.js'].source;
+                compilation.assets['service-worker.js'].source = function() {
+                    return swSource().replace(filenameTemplate, absUrl);
+                };
+                callback();
+            })
+            .catch((e) => callback(e));
+    });
+};
+
+module.exports = GenerateS3SWPrecachePlugin;
---
+++ src/service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));
