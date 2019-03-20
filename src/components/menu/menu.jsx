baa9c3c7f7161d4e3c09bd1086b34e0f8d167971
P3nny <p3nny@hacking.studio>
2019-03-20 12:49:10 +0100

    :sparkles: Revert teacher button

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -18,7 +18,6 @@ import buttonNew from '!raw-loader!../../../assets/icons/menu_plus.svg';
 import tabIconEdugames from '!raw-loader!../../../assets/icons/menu_edugames.svg';
 import tabIconProjects from '!raw-loader!../../../assets/icons/menu_projects.svg';
 import tabIconExamples from '!raw-loader!../../../assets/icons/menu_examples.svg';
-import buttonIconLehrerinnen from '!raw-loader!../../../assets/icons/menu_lehrer.svg';
 import buttonIconInfo from '!raw-loader!../../../assets/icons/menu_eltern-info.svg';
 import buttonIconMausseite from '!raw-loader!../../../assets/icons/menu_mausseite.svg';
 import buttonIconDatenschutz from '!raw-loader!../../../assets/icons/icon_hilfe.svg';
@@ -121,9 +120,6 @@ export const MenuComponent = (props) => {
                 </Tabs>
             </Box>
             <Box className={styles.buttonRow}>
-                <MenuButton iconSvg={buttonIconLehrerinnen} linkTo="/inhalte/lehrerinnen">
-                    Lehrer*innen
-                </MenuButton>
                 <MenuButton iconSvg={buttonIconInfo} linkTo="/inhalte/eltern">
                     Eltern-Info
                 </MenuButton>
