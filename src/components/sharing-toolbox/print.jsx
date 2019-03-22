3130dd23e93813caead363b03c5855644d1a1583
Christine <christine.gotth@gmail.com>
2019-03-22 14:21:43 +0100

    :sparkles: Add printing functionality

--- src/components/sharing-toolbox/print.jsx
+++ src/components/sharing-toolbox/print.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './print.css';
 import buttonBorder from './buttonBorder.svg';
 
-const PrintLayout = ({ stage }) => {
+const PrintLayout = ({ stage, layoutRef = React.createRef() }) => {
     return (
-        <div className={styles.printLayout}>
-            <div className={styles.button}>
-                <img
-                    className={styles.stage}
-                    src={stage}
-                />
-                <img
-                    className={styles.buttonBorder}
-                    src={buttonBorder}
-                />
-            </div>
+        <div className={styles.button} ref={layoutRef}>
+            <img
+                className={styles.stage}
+                src={stage}
+            />
+            <img
+                className={styles.buttonBorder}
+                src={buttonBorder}
+            />
         </div>
     );
+};
+
+PrintLayout.propTypes = {
+    stage: PropTypes.string.isRequired,
+    layoutRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
 };
 
 export default PrintLayout;
