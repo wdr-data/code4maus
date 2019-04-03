3b5fe92435de715a2309e4e652a9cc9d893adb28
P3nny <p3nny@hacking.studio>
2019-04-03 18:56:36 +0200

    :lipstick: Reorganize mobile screen

---
+++ src/components/mobile-screen/mobile-screen.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import MobilScreen from './mobile-screen.jsx';
+
+
+storiesOf('Mobile-Screen', module)
+    .add('default', () => (
+        <MobilScreen>
+        </MobilScreen>
+    ));
