4cc589b900dc6ff76371d1b0bf7254d562117778
Andreas Streichardt <andreas@mop.koeln>
2019-04-03 17:05:00 +0200

    ⚗ reuse backend module rules

--- webpack.netlify-functions.js
+++ webpack.netlify-functions.js
@@ -2,33 +2,24 @@
 const webpack = require('webpack');
 const fs = require('fs');
 const path = require('path');
-const babelConfig = require('./babel.backend');
 require('dotenv').config({ silent: true, path: '.env.backend' });
 
 const bucketSuffix = process.env.BRANCH === 'production' ? 'prod' : 'staging';
 const bucket = `${process.env.S3_BUCKET_PREFIX}-${bucketSuffix}`;
 
+const backendWebpack = require('./webpack.backend.js');
+
 module.exports = {
     entry: () =>
-        fs.readdirSync(path.join(__dirname, 'src/backend'))
+        fs
+            .readdirSync(path.join(__dirname, 'src/backend'))
             .filter((file) => !file.match(/^\./) && file.match(/\.js$/))
             .reduce((entries, file) => {
                 const name = file.replace(/\.js$/, '');
                 entries[name] = `./${name}`;
                 return entries;
             }, {}),
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: babelConfig,
-                },
-            },
-        ],
-    },
+    module: backendWebpack.module,
     plugins: [
         new webpack.DefinePlugin({
             'process.env.S3_BUCKET_PROJECTS': JSON.stringify(bucket),
