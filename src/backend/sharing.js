43bd0d195ec50f07ca7cac02b31cb7ca51daccca
Andreas Streichardt <andreas@mop.koeln>
2019-04-03 17:01:04 +0200

    🚑 oops

--- src/backend/sharing.js
+++ src/backend/sharing.js
@@ -1,17 +1,24 @@
 import fetch from 'node-fetch';
-import template from './template.ejs';
+import landingTemplate from './landingTemplate.ejs';
+import playerTamplate from './playerTemplate.ejs';
 
 const baseUrl = process.env.ASSET_BASEURL;
 
-export const handler = async (event, context, callback) => {
-    const { id } = event.queryStringParameters;
+const imageUrl = (id) => `/data/sharing/${id}`;
 
-    if (!id) {
-        return null;
-    }
+const playerFrame = (id) => {
+    const content = playerTamplate({ baseUrl, id, imageUrl: imageUrl(id) });
 
-    const imageUrl = `/data/sharing/${id}`;
+    return {
+        statusCode: 200,
+        headers: {
+            'content-type': 'text/html',
+        },
+        body: content,
+    };
+};
 
+const landingPage = async (host, id) => {
     const response = await fetch(baseUrl + '/manifest.json');
     const manifest = await response.json();
 
@@ -21,7 +28,13 @@ export const handler = async (event, context, callback) => {
         manifest['sharingpage.js'],
     ];
 
-    const content = template({ scripts, baseUrl, imageUrl });
+    const content = landingTemplate({
+        scripts,
+        baseUrl,
+        id,
+        host,
+        imageUrl: imageUrl(id),
+    });
 
     return {
         statusCode: 200,
@@ -30,4 +43,22 @@ export const handler = async (event, context, callback) => {
         },
         body: content,
     };
+};
+
+export const handler = async (event, context, callback) => {
+    const { Host } = event.headers;
+    const { id, frame } = event.queryStringParameters;
+
+    if (!id) {
+        return {
+            statusCode: 400,
+            body: 'Invalid request',
+        };
+    }
+
+    if (frame !== undefined) {
+        return playerFrame(id);
+    } else {
+        return landingPage(Host, id);
+    }
 };
