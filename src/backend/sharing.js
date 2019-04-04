f458a8dc74814dd9c48f6ef5f55858ee53e8a983
Andreas Streichardt <andreas@mop.koeln>
2019-04-04 12:13:26 +0200

    ✨ fetch image type from backend

--- src/backend/sharing.js
+++ src/backend/sharing.js
@@ -7,12 +7,33 @@ const apiHost = process.env.API_HOST;
 
 const imageUrl = (id) => `/data/sharing/${id}`;
 
-const playerFrame = (id) => {
+class HttpError {
+    constructor(code) {
+        this.code = code;
+    }
+
+    get message() {
+        switch (this.code) {
+        case 400:
+            return 'Bad request';
+        case 404:
+            return 'Not found';
+        case 500:
+            return 'Internal Server Error';
+        }
+        return 'unknown';
+    }
+}
+
+const playerFrame = async (id) => {
+    const type = await getImageType(id);
+
     const content = playerTamplate({
         baseUrl,
         apiHost,
         id,
         imageUrl: imageUrl(id),
+        type,
     });
 
     return {
@@ -22,6 +43,22 @@ const playerFrame = (id) => {
         },
         body: content,
     };
+};
+
+const getImageType = async (id) => {
+    const imageData = await fetch(`${baseUrl}/data/sharing/${id}`, {
+        method: 'head',
+    });
+
+    if (imageData.status === 404) {
+        throw new HttpError(404);
+    } else if (imageData.status >= 400) {
+        throw new Error(
+            `Got unexpected status code ${imageData.status} from backend`
+        );
+    }
+
+    return imageData.headers.get('content-type');
 };
 
 const landingPage = async (id) => {
@@ -34,11 +71,14 @@ const landingPage = async (id) => {
         manifest['sharingpage.js'],
     ];
 
+    const type = await getImageType(id);
+
     const content = landingTemplate({
         scripts,
         baseUrl,
         apiHost,
         id,
+        type,
         imageUrl: imageUrl(id),
     });
 
@@ -61,9 +101,32 @@ export const handler = async (event, context, callback) => {
         };
     }
 
-    if (frame === 'yes') {
-        return playerFrame(id);
-    } else {
-        return landingPage(id);
+    // to prevent "../shadow"
+    if (!id.match(/^[A-Za-z0-9_-]+$/)) {
+        return {
+            statusCode: 400,
+            body: 'Parameter invalid',
+        };
+    }
+
+    try {
+        if (frame === 'yes') {
+            return playerFrame(id);
+        } else {
+            return landingPage(id);
+        }
+    } catch (e) {
+        if (e instanceof HttpError) {
+            return {
+                statusCode: e.code,
+                body: e.message,
+            };
+        } else {
+            console.error(e);
+            return {
+                statusCode: 500,
+                body: 'Internal Server Error',
+            };
+        }
     }
 };
