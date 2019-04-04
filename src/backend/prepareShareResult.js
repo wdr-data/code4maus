a78693535da8a8bc6e029645dc1c067d5ba7799e
Marcus Weiner <marcus.weiner@gmail.com>
2019-04-04 13:18:34 +0200

    🔧 Use media type in sharing filename

--- src/backend/prepareShareResult.js
+++ src/backend/prepareShareResult.js
@@ -4,10 +4,20 @@ import nanoid from 'nanoid';
 const s3 = initS3();
 
 export const handler = async (event, context, callback) => {
+    const { type } = event.queryStringParameters;
+
+    if (!type) {
+        return {
+            statusCode: 400,
+            body: 'Parameter missing',
+        };
+    }
+
     const id = nanoid();
+    const sharingKey = `${id}.${encodeURIComponent(type)}`;
 
     const bucket = process.env.STORAGE_BUCKET || process.env.S3_BUCKET_PROJECTS;
-    const key = `data/sharing/${id}`;
+    const key = `data/sharing/${sharingKey}`;
     const params = {
         Bucket: bucket,
         Key: key,
@@ -21,7 +31,7 @@ export const handler = async (event, context, callback) => {
         },
         body: JSON.stringify({
             uploadUrl: presignedUrl,
-            sharingKey: id,
+            sharingKey,
         }),
     };
 };
