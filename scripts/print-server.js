7854be20e8169ff65df00233d54138a69f865acd
Christine <christine.gotth@gmail.com>
2019-04-18 12:40:30 +0200

    ♻️ Refactor to using local print proxy

---
+++ scripts/print-server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const multer = require('multer');
+const printer = require('printer');
+const cors = require('cors');
+
+const app = express();
+
+const fileStorage = multer.memoryStorage();
+const upload = multer({ storage: fileStorage });
+
+app.use(cors());
+
+app.post('/', upload.single('button'), (req, res) => {
+    console.log(req.file.buffer);
+    printer.printDirect({
+        data: req.file.buffer,
+        type: 'PDF',
+        success: (id) => {
+            res.send(`Printed with id: ${id}`);
+        },
+        error: (err) => {
+            res.status(500).send(`error on printing: ${err}`);
+        },
+    });
+});
+
+app.listen(8602, () => {
+    console.log('Example app listening on port 8602!');
+});
