e2105590b94574760ae531d34f5bed22ad6ec2a2
Marcus Weiner <marcus.weiner@gmail.com>
2019-05-01 13:15:27 +0200

    🥚 Hide offline support behind feature flag
    Co-authored-by: christinegotth <christine.gotth@gmail.com>

--- src/lib/feature-flags.js
+++ src/lib/feature-flags.js
@@ -2,6 +2,7 @@ import { useMemo } from 'react';
 export const FEATURE_GAMESPREVIEW = 'gamesPreview';
 export const FEATURE_SHARING = 'sharing';
 export const FEATURE_PRINTING = 'printing';
+export const FEATURE_OFFLINE = 'offline';
 
 export const useFeatureFlag = (feature) => {
     const enabled = useMemo(() => localStorage.getItem(`feature/${feature}`), [ feature ]);
