db7930e40161de9a4e370bdd844f1bbfc20bfd26
Marcus Weiner <marcus.weiner@gmail.com>
2019-05-03 12:11:30 +0200

    🏁 Enable printing on windows using Acrobat Reader

--- scripts/print-server.js
+++ scripts/print-server.js
@@ -1,26 +1,62 @@
 const express = require('express');
 const multer = require('multer');
-const printer = require('printer');
 const cors = require('cors');
+const path = require('path');
+const child_process = require('child_process');
+const fs = require('fs');
 
 const app = express();
 
-const fileStorage = multer.memoryStorage();
-const upload = multer({ storage: fileStorage });
+const upload = multer({ dest: 'code4maus-uploads/' });
 
 app.use(cors());
 
-app.post('/', upload.single('button'), (req, res) => {
-    printer.printDirect({
-        data: req.file.buffer,
-        type: 'PDF',
-        success: (id) => {
-            res.send(`Printed with id: ${id}`);
-        },
-        error: (err) => {
-            res.status(500).send(`error on printing: ${err}`);
-        },
-    });
+const printDefault = (buffer, format = 'PDF') => {
+    const printer = require('printer');
+    return new Promise((res, rej) => printer.printDirect({
+        data: buffer,
+        type: format,
+        success: res,
+        error: rej,
+    }));
+};
+
+const binPath = path.join(
+    'C:', 'Program Files (x86)',
+    'Adobe', 'Reader 11.0', 'Reader', 'AcroRd32.exe'
+);
+try {
+    const binStat = fs.statSync(binPath);
+    if (!binStat.isFile) {
+        throw new Error('Acrobat Reader executable is not a file!');
+    }
+} catch (err) {
+    console.error(`Acrobat Reader is required at ${binPath}!`, 'Failed with:', err);
+}
+
+const printWin = async (filepath) => new Promise((res, rej) => child_process.exec(
+    `"${binPath}" /t "${path.join(process.cwd(), filepath)}"`,
+    (err, stdout, stderr) => {
+        if (err) {
+            console.error('failed:', err, stderr);
+            return rej(err);
+        }
+        res(stdout);
+    }
+));
+
+app.post('/', upload.single('button'), async (req, res) => {
+    try {
+        let printId;
+        if (process.platform !== 'win32') {
+            printId = await printDefault(req.file.buffer);
+        } else {
+            printId = await printWin(req.file.path);
+        }
+        res.send(`Printed with id: ${printId}`);
+    } catch (err) {
+        res.status(500).send(`error on printing: ${err}`);
+    }
 });
 
 app.listen(8602, () => {
