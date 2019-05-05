adc06ff2e60b98ddb88d5d51409596753708fa0f
Marcus Weiner <marcus.weiner@gmail.com>
2019-05-05 14:28:51 +0200

    🚀 Upload print server to gh-pages

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-# Build
-
-yarn build:patternlib
-
-
-# Deploy
+SRC_DIR=$1
+DEST_DIR=$2
+FLAGS=$3
 
 export AUTHOR_EMAIL="$(git log -1 $TRAVIS_COMMIT --pretty="%aE")"
 export AUTHOR_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
@@ -15,7 +12,8 @@ chmod 600 /tmp/deploy_key
 eval "$(ssh-agent -s)"
 ssh-add /tmp/deploy_key
 
-yarn gh-pages -d .patternlib \
-              -m "Update patternlib from ${TRAVIS_BRANCH}" \
+yarn gh-pages -d ${SRC_DIR} -e ${DEST_DIR} \
+              -m "Upload ${SRC_DIR} from ${TRAVIS_BRANCH}" \
               -r git@github.com:${TRAVIS_REPO_SLUG}.git \
-              -u "${AUTHOR_NAME} <${AUTHOR_EMAIL}>"
+              -u "${AUTHOR_NAME} <${AUTHOR_EMAIL}>" \
+              ${FLAGS}
