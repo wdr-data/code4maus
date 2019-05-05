46fe1bf10a751df3305984e256d2c290854b5e73
Marcus Weiner <marcus.weiner@gmail.com>
2019-05-05 18:18:56 +0200

    🚑 Disable cache for all index files

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -12,5 +12,7 @@ else
 fi
 
 aws s3 sync build s3://${BUCKET} --delete --acl public-read
-aws s3 cp build/index.html s3://${BUCKET}/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
+for FILE in $(find build -name index.html -printf '%P\n'); do
+    aws s3 cp build/${FILE} s3://${BUCKET}/${FILE} --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
+done
 aws s3 cp build/service-worker.js s3://${BUCKET}/service-worker.js --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type application/javascript --acl public-read
