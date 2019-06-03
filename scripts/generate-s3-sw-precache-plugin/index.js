4b9227ae38f13826e859bdfe0705498e6c50ab3b
Marcus Weiner <marcus.weiner@gmail.com>
2019-06-03 17:25:08 +0200

    ⚡️ Reduce service worker cache by deduping default project

--- scripts/generate-s3-sw-precache-plugin/index.js
+++ scripts/generate-s3-sw-precache-plugin/index.js
@@ -1,5 +1,25 @@
+require('@babel/polyfill');
+require('@babel/register')({
+    configFile: false,
+    babelrc: false,
+    presets: [
+        ['@babel/env',
+            { 'targets': { 'node': 8 } },
+        ],
+    ],
+    plugins: [
+        ['import-noop',
+            { 'extensions': [ 'mp3', 'svg', 'jpg' ] },
+        ],
+    ],
+});
+
 const crypto = require('crypto');
-const { getAllAssets } = require('../../scripts/lib/assets');
+const { getAllAssets } = require('../lib/assets');
+const defaultAssets = require('../../src/lib/default-project/').default;
+const defaultAssetIDs = defaultAssets.map(
+    (asset) => `${asset.id}.${asset.dataFormat.toLowerCase()}`
+);
 
 function GenerateS3SWPrecachePlugin(options) {
     this.filename = options.filename || 's3-manifest.[hash].js';
@@ -9,6 +29,9 @@ GenerateS3SWPrecachePlugin.prototype.apply = function(compiler) {
     let filenameTemplate = this.filename;
     compiler.plugin('emit', function(compilation, callback) {
         getAllAssets()
+            .then((files) => files.filter(
+                (file) => !defaultAssetIDs.includes(file)
+            ))
             .then((files) => {
                 const list = JSON.stringify(files.map((file) => '/data/assets/' + file));
                 const hash = crypto.createHash('md5').update(list, 'utf8').digest('hex');
