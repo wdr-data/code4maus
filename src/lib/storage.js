bc12fd43c4baeea6549e4ad498b92179842c7032
Marcus Weiner <marcus.weiner@gmail.com>
2019-06-03 16:12:33 +0200

    🔥 Remove loading of legacy scratch assets

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -2,10 +2,6 @@ import ScratchStorage from 'scratch-storage';
 
 import defaultProjectAssets from './default-project';
 
-const PROJECT_SERVER = 'https://projects.scratch.mit.edu';
-const ASSET_SERVER = 'https://cdn.assets.scratch.mit.edu';
-
-
 export const s3assets = (filename) => `/data/assets/${filename}`;
 export const s3userFile = (userId, path) => `/data/projects/${userId}/${path}`;
 
@@ -20,7 +16,6 @@ class Storage extends ScratchStorage {
 
         this.setupEduSource();
         this.setupS3Source();
-        this.setupScratchLegacySources();
 
         this.cacheDefaultProject();
     }
@@ -53,29 +48,6 @@ class Storage extends ScratchStorage {
         this.addWebStore(
             [ this.AssetType.ImageVector, this.AssetType.ImageBitmap, this.AssetType.Sound ],
             (asset) => s3assets(`${asset.assetId}.${asset.dataFormat}`)
-        );
-    }
-
-    setupScratchLegacySources() {
-        this.addWebStore(
-            [ this.AssetType.Project ],
-            (projectAsset) => {
-                if (projectAsset.assetId === 0) {
-                    return false;
-                }
-                const [ projectId, revision ] = projectAsset.assetId.split('.');
-                return revision ?
-                    `${PROJECT_SERVER}/internalapi/project/${projectId}/get/${revision}` :
-                    `${PROJECT_SERVER}/internalapi/project/${projectId}/get/`;
-            }
-        );
-        this.addWebStore(
-            [ this.AssetType.ImageVector, this.AssetType.ImageBitmap, this.AssetType.Sound ],
-            (asset) => `${ASSET_SERVER}/internalapi/asset/${asset.assetId}.${asset.dataFormat}/get/`
-        );
-        this.addWebStore(
-            [ this.AssetType.Sound ],
-            (asset) => `static/extension-assets/scratch3_music/${asset.assetId}.${asset.dataFormat}`
         );
     }
 
