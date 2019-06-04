e0fa78b0df1ec53f8012b4c867a7779cba848b26
Marcus Weiner <marcus.weiner@gmail.com>
2019-06-04 15:51:51 +0200

    ⚡️ Render mobile overlay lazily

--- src/containers/mobile-screen.jsx
+++ src/containers/mobile-screen.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import debounce from 'lodash.debounce';
 
-import MobileScreenComponent from '../components/mobile-screen/mobile-screen.jsx';
 import indexStyles from '../css/index.css';
+import LazyRender from './lazy-render.jsx';
 
 class MobileScreen extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            isMobile: window.innerWidth < 600,
+        };
+
         this.el = document.createElement('div');
         this.el.className = indexStyles.mobileOverlayRoot;
+
+        this.detectMobileSizeDebounce = debounce(this.detectMobileSize.bind(this), 500);
     }
 
     componentDidMount() {
         document.body.appendChild(this.el);
+        window.addEventListener('resize', this.detectMobileSizeDebounce);
     }
 
     componentWillUnmount() {
+        window.removeEventListener('resize', this.detectMobileSizeDebounce);
         document.body.removeChild(this.el);
     }
 
+    detectMobileSize(ev) {
+        const isMobile = window.innerWidth < 600;
+        if (this.state.isMobile !== isMobile) {
+            this.setState({ isMobile });
+        }
+    }
+
     render() {
+        if (!this.state.isMobile) {
+            return null;
+        }
+
         return ReactDOM.createPortal(
-            <MobileScreenComponent />,
+            <LazyRender promise={import('../components/mobile-screen/mobile-screen.jsx')} />,
             this.el,
         );
     }
