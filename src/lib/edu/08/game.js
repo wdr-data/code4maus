79f536650602853181e170c3e202775dad02cf50
P3nny <p3nny@hacking.studio>
2019-06-05 12:30:01 +0200

    🍱 Adjust game.js and delete unused assets

--- src/lib/edu/08/game.js
+++ src/lib/edu/08/game.js
@@ -1,53 +1,52 @@
 import aStadtLandEnte from './assets/StadtLandEnte.png';
-import a12 from './assets/12.png';
-import a01 from './assets/01.png';
-import a01_1 from './assets/01_1.png';
-import avariablen_hand from './assets/variablen_hand.png';
+import a12 from './assets/12.jpg';
 import a02 from '../shared_assets/variablen_hand.png';
-import a03_1 from './assets/03_1.png';
-import a03_02 from './assets/03_02.png';
-import a05 from './assets/05.png';
-import astart from './assets/start.png';
-import ax03 from './assets/x03.png';
-import asteuerung from './assets/steuerung.png';
-import a08 from './assets/08.png';
-import aoperatoren from './assets/operatoren.png';
-import a09 from './assets/09.png';
-import a10 from './assets/10.png';
-import a11 from './assets/11.png';
-import axx2 from './assets/xx2.png';
-import agut_gemacht from './assets/gut_gemacht.png';
-import aspannung from './assets/spannung.png';
-import axx1 from './assets/xx1.png';
-import a16 from './assets/16.png';
-import a14 from './assets/14.png';
-import aXX3 from './assets/XX3.png';
-import aXX4 from './assets/XX4.png';
-import a18 from './assets/18.png';
-import a19 from './assets/19.png';
-import ak1 from './assets/k1.png';
-import ac from './assets/c.png';
+import b02 from './assets/02.jpg'
+import a03_1 from './assets/03_1.jpg';
+import a03_02 from './assets/03_02.jpg';
+import a03 from './assets/03.jpg';
+import a05 from './assets/05.jpg';
+import astart from '../shared_assets/start.png';
+import ax03 from './assets/x03.jpg';
+import asteuerung from '../shared_assets/steuerung.jpg';
+import a08 from './assets/08.jpg';
+import aoperatoren from '../shared_assets/operatoren.jpg';
+import a09 from './assets/09.jpg';
+import a10 from './assets/10.jpg';
+import a11 from './assets/11.jpg';
+import axx2 from './assets/xx2.jpg';
+import agut_gemacht from '../shared_assets/gut_gemacht.png';
+import aspannung from './assets/spannung.jpg';
+import axx1 from './assets/xx1.jpg';
+import a16 from './assets/16.jpg';
+import a14 from './assets/14.jpg';
+import aXX3 from './assets/XX3.jpg';
+import aXX4 from './assets/XX4.jpg';
+import a18 from './assets/18.jpg';
+import a19 from './assets/19.jpg';
+import ak1 from './assets/k1.jpg';
+import ac from './assets/c.jpg';
 import a20_2 from './assets/20_2.png';
-import a21 from './assets/21.png';
-import aherz from './assets/herz.png';
+import a21 from './assets/21.jpg';
+import aherz from '../shared_assets/herz.png';
 import ak3 from './assets/k3.png';
-import a23 from './assets/23.png';
-import a24 from './assets/24.png';
-import a25 from './assets/25.png';
-import a26 from './assets/26.png';
-import a27 from './assets/27.png';
-import a28 from './assets/28.png';
-import a29 from './assets/29.png';
-import a30 from './assets/30.png';
-import a31 from './assets/31.png';
-import a32 from './assets/32.png';
-import a33_1 from './assets/33_1.png';
-import a33_2 from './assets/33_2.png';
-import aq1 from './assets/q1.mp4';
-import a36 from './assets/36.png';
-import a37 from './assets/37.png';
-import a38 from './assets/38.png';
-import a39 from './assets/39.png';
+import a23 from './assets/23.jpg';
+import a24 from './assets/24.jpg';
+import a25 from './assets/25.jpg';
+import a26 from './assets/26.jpg';
+import a27 from './assets/27.jpg';
+import a28 from './assets/28.jpg';
+import a29 from './assets/29.jpg';
+import a30 from './assets/30.jpg';
+import a31 from './assets/31.jpg';
+import a32 from './assets/32.jpg';
+import a33_1 from './assets/33_1.jpg';
+import a33_2 from './assets/33_2.jpg';
+import aq1 from './assets/q1.gif';
+import a36 from './assets/36.jpg';
+import a37 from './assets/37.jpg';
+import a38 from './assets/38.jpg';
+import a39 from './assets/39.jpg';
 import aStern from '../shared_assets/super.png';
 
 export default {
@@ -65,19 +64,11 @@ export default {
             'caption': 'Programmiere ein Stadt-Land-Fluss-Spiel.',
         },
         {
-            'asset': a01,
-            'caption': "Füge die Figur 'Ente' hinzu",
-        },
-        {
-            'asset': a01_1,
-            'caption': "Lösche die Figur 'Maus'",
-        },
-        {
-            'asset': avariablen_hand,
+            'asset': a02,
             'caption': "Klicke auf 'Variablen'",
         },
         {
-            'asset': a02,
+            'asset': b02,
             'caption': "Erstelle eine neue Liste, nenne sie 'Wörter'",
         },
         {
@@ -89,7 +80,7 @@ export default {
             'caption': 'Verlängere dein Programm: ',
         },
         {
-            'asset': a01,
+            'asset': a03,
             'caption': "Füge 'Antwort' hinzu",
         },
         {
@@ -146,10 +137,6 @@ export default {
         {
             'asset': aspannung,
             'caption': 'Damit das Spiel spannender wird, füge einen Countdown dazu...',
-        },
-        {
-            'asset': avariablen_hand,
-            'caption': "Klicke auf 'Variablen'",
         },
         {
             'asset': axx1,
@@ -216,7 +203,7 @@ export default {
             'caption': "Mache noch ein 'Wenn'-Programm",
         },
         {
-            'asset': avariablen_hand,
+            'asset': a02,
             'caption': "Klicke auf 'Variablen'",
         },
         {
@@ -256,7 +243,7 @@ export default {
             'caption': "Gut! Jetzt änderst Du, dass ein zufälliger Buchstabe statt 'E' gefragt wird.",
         },
         {
-            'asset': avariablen_hand,
+            'asset': a02,
             'caption': "Klicke auf 'Variablen'",
         },
         {
