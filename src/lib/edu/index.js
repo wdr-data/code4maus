b30782a65bafbf5aec99833e9709e9e2e74483f1
P3nny <p3nny@hacking.studio>
2019-06-05 15:39:34 +0200

    🚚 Restructue order of edu games

--- src/lib/edu/index.js
+++ src/lib/edu/index.js
@@ -1,5 +1,3 @@
-import game06 from './06/game.js';
-import game04_c from './04_c/game.js';
 import game00 from './00/game.js';
 import game01 from './01/game.js';
 import game02 from './02/game.js';
@@ -7,13 +5,12 @@ import game03 from './03/game.js';
 import game04 from './04/game.js';
 import game04_b from './04_b/game.js';
 import game07 from './07/game.js';
+import game04_c from './04_c/game.js';
 import game05 from './05/game.js';
 import game08 from './08/game.js';
 import examples from './examples.js';
 
 const games = [
-    game06,
-    game04_c,
     game00,
     game01,
     game02,
@@ -21,6 +18,7 @@ const games = [
     game04,
     game04_b,
     game07,
+    game04_c,
     game05,
     game08,
 ];
