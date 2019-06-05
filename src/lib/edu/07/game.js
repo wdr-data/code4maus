507bcf3de358e9779fa1f90565a36216c6127908
Christine <christine.gotth@gmail.com>
2019-06-05 17:24:17 +0200

    🎨 Restructure edu game 07

--- src/lib/edu/07/game.js
+++ src/lib/edu/07/game.js
@@ -1,35 +1,40 @@
 import a00 from './assets/00.jpg';
 import a001 from './assets/001.jpg';
 import a1 from './assets/1.jpg';
-import a2 from './assets/2.mp4';
-import a3 from './assets/3.mp4';
-import a3_1 from './assets/3.1.mp4';
-import a5_1 from './assets/5.1.jpg';
-import a5_2 from './assets/5.2.jpg';
-import a6_1 from './assets/6.1.jpg';
-import a4_1 from './assets/4.1.mp4';
-import a6_2 from './assets/6.2.jpg';
-import a6_4 from './assets/6.4.jpg';
-import a6_5 from './assets/6.5.jpg';
-import a6_6 from './assets/6.6.jpg';
-import a7 from './assets/7.jpg';
-import a9_1 from './assets/9.1.jpg';
-import a10_1 from './assets/10.1.mp4';
-import a10_2 from './assets/10.2.mp4';
-import a10_3 from './assets/10.3.mp4';
-import a11 from './assets/11.mp4';
-import a12 from './assets/12.jpg';
-import a13 from './assets/13.jpg';
-import a14 from './assets/14.jpg';
-import a15_1 from './assets/15.1.mp4';
-import a15_2 from './assets/15.2.jpg';
-import a16 from './assets/16.jpg';
-import a15_3 from './assets/15.3.mp4';
-import a17_1 from './assets/17.1.mp4';
-import a17_2 from './assets/17.2.jpg';
-import a19 from './assets/19.jpg';
-import a20 from './assets/20.jpg';
-import a17 from './assets/17.mp4';
+import a2 from './assets/1.mp4';
+import a3 from './assets/2.mp4';
+import a4 from './assets/3.mp4';
+import a5 from './assets/n1.png';
+import a6 from './assets/3.jpg';
+import a7 from './assets/n2.mp4';
+import a8 from './assets/n3.png';
+import a9 from './assets/n4.mp4';
+import a10 from './assets/n5.png';
+import a11 from './assets/n6.mp4';
+import a12 from './assets/n7.png';
+import a13 from '../shared_assets/start.jpg';
+import a14 from './assets/n8.mp4';
+import a15 from './assets/7.jpg';
+import a16 from './assets/9.1.jpg';
+import a17 from './assets/10.1.mp4';
+import a18 from './assets/10.2.mp4';
+import a19 from './assets/10.3.mp4';
+import a20 from './assets/11.mp4';
+import a21 from './assets/12.jpg';
+import a22 from './assets/13.jpg';
+import a23 from './assets/14.jpg';
+import a24 from '../04_b/assets/blitz.svg';
+import a25 from './assets/15.1.mp4';
+import a26 from './assets/15.2.jpg';
+import a27 from './assets/16.jpg';
+import a28 from './assets/15.3.mp4';
+import a29 from './assets/17.1.mp4';
+import a30 from './assets/17.2.jpg';
+import a31 from './assets/19.jpg';
+import a32 from './assets/20.jpg';
+import a33 from '../shared_assets/herz.png';
+import a34 from './assets/n9.mp4';
+import a35 from '../shared_assets/gut_gemacht.png';
 
 export default {
     id: '07',
@@ -52,147 +57,143 @@ export default {
             'caption': 'Klickt auf Variablen.',
         },
         {
-            'asset': a1,
+            'asset': a2,
             'caption': "Erstellt zwei neue Listen. Nennt sie 'Wörter'...",
         },
         {
-            'asset': a2,
+            'asset': a3,
             'caption': "und 'Geraten'.",
         },
         {
-            'asset': a3,
+            'asset': a4,
             'caption': "Fügt eine neue Variable hinzu. Nennt sie 'A-Z'.",
         },
         {
-            'asset': a2,
-            'caption': 'Baut diesen Block.',
+            'asset': a5,
+            'caption': 'Baut dieses Programm.',
         },
         {
-            'asset': a3,
+            'asset': a6,
             'caption': 'Klickt auf Operatoren.',
         },
         {
-            'asset': a3_1,
-            'caption': "Ändert 'meine Variable zu 'A-Z' und fügt den Block 'Zwischen ... und ...' hinzu'.",
+            'asset': a7,
+            'caption': "Zieht 'Zeichen von' rein.",
         },
         {
-            'asset': a5_1,
+            'asset': a8,
             'caption': 'Diese Buchstaben können vorkommen.',
         },
         {
-            'asset': a5_2,
-            'caption': 'Der Buchstabe wird zufällig ausgewählt, ändert die Zahl 10 zu 6.',
+            'asset': a9,
+            'caption': 'Der Buchstabe wird zufällig ausgewählt.',
         },
         {
-            'asset': a6_1,
-            'caption': 'Wiederholt bis...',
-        },
-        {
-            'asset': a4_1,
-            'caption': '... die Liste der Wörter gleich 3 ist.',
-        },
-        {
-            'asset': a6_2,
-            'caption': "Bei 'Fühlen': 'Frage ... und warte'",
-        },
-        {
-            'asset': a6_4,
-            'caption': "Verbindt 'Ein Tier mit ' und die Variable 'A-Z'.",
-        },
-        {
-            'asset': a6_5,
-            'caption': "Fügt 'Ding' zu 'Geraten' hinzu.",
-        },
-        {
-            'asset': a6_6,
-            'caption': "Ersetzt 'Ding' durch 'Antwort'.",
-        },
-        {
-            'caption': 'Probiert mal!',
-        },
-        {
-            'asset': a7,
-            'caption': 'Baut ein zweites Programm,',
-        },
-        {
-            'asset': a9_1,
-            'caption': '... damit das zweite Kind raten kann.',
-        },
-        {
-            'asset': a10_1,
-            'caption': 'So soll die Frage aussehen.',
-        },
-        {
-            'asset': a10_2,
-            'caption': "Für die Antwort: Zieht 'Enthält' rein.",
-        },
-        {
-            'asset': a10_3,
-            'caption': "Und 'Antwort' aus der Kategorie 'Fühlen'",
+            'asset': a10,
+            'caption': 'Ändert die Zahl.',
         },
         {
             'asset': a11,
-            'caption': 'Wurde richtig geraten? Wenn ja:',
+            'caption': "Zieht 'Verbinde' rein.",
         },
         {
             'asset': a12,
-            'caption': "Die Antwort wird in die 'Geraten' Liste geschrieben.",
+            'caption': 'Das soll die Ente fragen:',
         },
         {
             'asset': a13,
-            'caption': 'Wenn die Antwort falsch ist:',
-        },
-        {
-            'asset': a14,
             'caption': 'Testet euer Programm.',
         },
         {
-            'caption': 'Man kann nur einmal raten!',
+            'asset': a14,
+            'caption': 'Schreibt 3 Tiere in die Liste.',
         },
         {
-            'asset': a15_1,
-            'caption': "Benutzt 'Wiederhole bis'",
-        },
-        {
-            'asset': a15_2,
-            'caption': "Zieht den 'x = 100' Operator rein.",
+            'asset': a15,
+            'caption': 'Baut ein zweites Programm,',
         },
         {
             'asset': a16,
-            'caption': "Zieht 2 mal 'Länge von' dazu.",
-        },
-        {
-            'asset': a15_3,
-            'caption': 'So werden die Listen verglichen.',
-        },
-        {
-            'asset': a17_1,
-            'caption': "Zieht 'sage' ganz nach unten.",
-        },
-        {
-            'asset': a17_2,
-            'caption': 'Ändert den Text.',
-        },
-        {
-            'asset': a19,
-            'caption': "Zieht 'zeige' und 'verstecke' in euer Programm.",
-        },
-        {
-            'asset': a20,
-            'caption': "Zieht 2 mal 'lösche alles' rein.",
-        },
-        {
-            'caption': 'So könnt ihr immer wieder spielen.',
-        },
-        {
-            'asset': a16,
-            'caption': 'Spieler 1 klickt auf > und schreibt 3 Tiere in die Liste.',
+            'caption': '... damit das zweite Kind raten kann.',
         },
         {
             'asset': a17,
+            'caption': 'So soll die Frage aussehen.',
+        },
+        {
+            'asset': a18,
+            'caption': "Für die Antwort: Zieht 'Enthält' rein.",
+        },
+        {
+            'asset': a19,
+            'caption': "Und 'Antwort' aus der Kategorie 'Fühlen'",
+        },
+        {
+            'asset': a20,
+            'caption': 'Wurde richtig geraten? Wenn ja:',
+        },
+        {
+            'asset': a21,
+            'caption': "Die Antwort wird in die 'Geraten' Liste geschrieben.",
+        },
+        {
+            'asset': a22,
+            'caption': 'Wenn die Antwort falsch ist:',
+        },
+        {
+            'asset': a23,
+            'caption': 'Testet euer Programm.',
+        },
+        {
+            'asset': a24,
+            'caption': 'Man kann nur einmal raten!',
+        },
+        {
+            'asset': a25,
+            'caption': "Benutzt 'Wiederhole bis'",
+        },
+        {
+            'asset': a26,
+            'caption': "Zieht den 'x = 100' Operator rein.",
+        },
+        {
+            'asset': a27,
+            'caption': "Zieht 2 mal 'Länge von' dazu.",
+        },
+        {
+            'asset': a28,
+            'caption': 'So werden die Listen verglichen.',
+        },
+        {
+            'asset': a29,
+            'caption': "Zieht 'sage' ganz nach unten.",
+        },
+        {
+            'asset': a30,
+            'caption': 'Ändert den Text.',
+        },
+        {
+            'asset': a31,
+            'caption': "Zieht 'zeige' und 'verstecke' in eure Programme.",
+        },
+        {
+            'asset': a32,
+            'caption': "Zieht 2 mal 'lösche alles' rein.",
+        },
+        {
+            'asset': a33,
+            'caption': 'So könnt ihr immer wieder spielen.',
+        },
+        {
+            'asset': a14,
+            'caption': 'Spieler 1 klickt auf > und schreibt 3 Tiere in die Liste.',
+        },
+        {
+            'asset': a34,
             'caption': 'Spieler 2 klickt auf die Ente und muss raten.',
         },
         {
+            'asset': a35,
             'caption': 'Viel Spaß beim Spielen!',
         },
     ],
