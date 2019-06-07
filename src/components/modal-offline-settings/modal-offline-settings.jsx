e37cea104c386890823ec3df0fbe9512bae47fe2
P3nny <p3nny@hacking.studio>
2019-06-07 15:00:09 +0200

    ✨ Make offline modal usable.

--- src/components/modal-offline-settings/modal-offline-settings.jsx
+++ src/components/modal-offline-settings/modal-offline-settings.jsx
@@ -1,35 +1,62 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Modal from '../modal/modal.jsx';
 import Button from '../button/button.jsx';
 import { Spinner } from '../loader/loader.jsx';
+import { setEnabled } from '../../reducers/offline';
 
 import styles from './modal-offline-settings.css';
 
-export const modes = [ 'inactive', 'activating', 'unsupported', 'error', 'active' ];
+const getMode = (props) => {
+    const offlineSupport = 'serviceWorker' in navigator;
+    if (!offlineSupport) {
+        return 'unsupported';
+    }
+
+    if (!props.enabled) {
+        return 'inactive';
+    }
+
+    if (props.installing) {
+        return 'installing';
+    }
+
+    if (props.installed) {
+        return 'installed';
+    }
+
+    if (props.installError !== null) {
+        return 'error';
+    }
+
+    return 'inactive';
+};
+
+export const modes = [ 'inactive', 'installing', 'unsupported', 'error', 'installed' ];
 
 const actions = (props) => {
-    switch (props.mode) {
+    switch (getMode(props)) {
     case 'inactive':
         return (
             <React.Fragment>
-                <span>Status: Offline aus ...</span>
+                <span>Status: Offline aus.</span>
                 <div className={styles.actionsButton}>
-                    <Button style='primary'>
-                Aktivieren
+                    <Button style='primary' onClick={props.setEnabled}>
+                        Aktivieren
                     </Button>
                 </div>
             </React.Fragment>
         );
-    case 'activating':
+    case 'installing':
         return (
             <React.Fragment>
                 <span>Status: Wird aktiviert ...</span>
 
                 <div className={styles.actionsButton}>
-                    <Button style='secondary'>
-                    Abbrechen
+                    <Button style='secondary' onClick={props.setDisabled}>
+                        Abbrechen
                     </Button>
                 </div>
                 <div className={styles.actionsSpinner}>
@@ -47,20 +74,20 @@ const actions = (props) => {
             <span>Status: Upps, da ist etwas schief gelaufen.</span>
             <div className={styles.actionsButton}>
                 <Button style='primary'>
-                Noch mal versuchen
+                    Noch mal versuchen
                 </Button>
             </div>
         </React.Fragment>);
 
-    case 'active':
+    case 'installed':
         return (<React.Fragment>
             <span>Status: Offline an. </span>
             <div className={styles.actionsButton}>
-                <Button style='primary'>
-                Aktualisieren
-                </Button>
-                <Button style='primary'>
-                Deaktivieren
+                { /* <Button style='primary'>
+                    Aktualisieren
+                  </Button> */}
+                <Button style='primary' onClick={props.setDisabled}>
+                    Deaktivieren
                 </Button>
             </div>
 
@@ -70,7 +97,7 @@ const actions = (props) => {
 };
 
 const ModalOfflineSettings = (props) =>
-    <Modal contentLabel='Offline-Modus'>
+    <Modal contentLabel='Offline-Modus' onRequestClose={props.onRequestClose} >
         <div className={styles.content}>
             <section className={styles.description}>
                 <p>Der Offlinemodus ist für Chrome, Firefox und Safari verfügbar.</p>
@@ -79,11 +106,28 @@ const ModalOfflineSettings = (props) =>
                 <p>Beispiele sind im Offline-Modus nicht verfügbar.</p>
             </section>
 
-
             <section className={styles.actions}>
                 {actions(props)}
             </section>
         </div>
     </Modal>;
 
-export default ModalOfflineSettings;
+ModalOfflineSettings.propTypes = {
+    onRequestClose: PropTypes.func,
+    setEnabled: PropTypes.func.isRequired,
+    setDisabled: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    installed: state.scratchGui.offline.installed,
+    installError: state.scratchGui.offline.installError,
+    installing: state.scratchGui.offline.installing,
+    enabled: state.scratchGui.offline.enabled,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    setEnabled: () => dispatch(setEnabled(true)),
+    setDisabled: () => dispatch(setEnabled(false)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ModalOfflineSettings);
--- src/components/offline-support/offline-support.jsx
+++ src/components/offline-support/offline-support.jsx
@@ -1,61 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import ButtonComponent from '../button/button.jsx';
+import Button from '../button/button.jsx';
 import { connect } from 'react-redux';
-import { setEnabled } from '../../reducers/offline.js';
+import ModalOfflineSettings from '../modal-offline-settings/modal-offline-settings.jsx';
 
+import styles from './offline-support.css';
 
 const OfflineSupport = (props) => {
-    const offlineSupport = 'serviceWorker' in navigator;
-    if (!offlineSupport) {
-        return null;
-    }
-
-    if (props.installing) {
-        return (
-            <div>Installiere...</div>
-        );
-    }
-
-    if (props.installed) {
-        return (
-            <div>Offlineversion aktiviert</div>
-        );
-    }
-
-    if (!props.enabled) {
-        return (
-            <ButtonComponent style="link" onClick={() => props.setEnabled(true)}>Offlinemodus aktivieren</ButtonComponent>
-        );
-    }
-
-    if (props.installError !== null) {
-        return (
-            <div>Fehler: {props.installError}</div>
-        );
-    }
-
-    return null;
+    const [ isOfflineSettingsOpen, setOfflineSettingsOpen ] = useState(false);
+    return (
+        <React.Fragment>
+            {isOfflineSettingsOpen && < ModalOfflineSettings
+                onRequestClose={() => setOfflineSettingsOpen(false)}
+            />}
+            <div className={styles.offlineStatus}>
+                <Button style='secondary' onClick={() => setOfflineSettingsOpen(true)}>
+    Offline-Modus
+                </Button>
+                <span>
+                    {props.installed?'An':'Aus'}
+                </span>
+            </div>
+        </React.Fragment>);
 };
 
 OfflineSupport.propTypes = {
-    setEnabled: PropTypes.func.isRequired,
     installed: PropTypes.bool.isRequired,
-    installError: PropTypes.string,
-    installing: PropTypes.bool.isRequired,
-    enabled: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = (state) => ({
     installed: state.scratchGui.offline.installed,
-    installError: state.scratchGui.offline.installError,
-    installing: state.scratchGui.offline.installing,
-    enabled: state.scratchGui.offline.enabled,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    setEnabled: (value) => dispatch(setEnabled(value)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(OfflineSupport);
+export default connect(mapStateToProps)(OfflineSupport);
