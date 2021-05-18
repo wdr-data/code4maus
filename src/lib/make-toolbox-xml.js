f3ccad1e027497215a01b2e8ee4ba930fb956fd9
Bonflintstone <freddy@railslove.com>
2021-05-18 16:11:12 +0200

    Starts with adding music blocks

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -1085,6 +1085,52 @@ const operators = prepareBlocks(
 `
 )
 
+const musicBlockLib = () => ({
+  play_drum_for_beats: `
+        <block type="extension_music_drum" id="extension_music_drum">
+          <value name="DRUM">
+            <shadow type="drum">
+              <field name="DRUM">1</field>
+            </shadow>
+          </value>
+          <value name="DRUM">
+            <shadow type="math_number">
+              <field name="NUM">1</field>
+            </shadow>
+          </value>
+        </block>
+  `,
+  play_note: `
+    <block type="extension_music_play_note" id="extension_music_play_note"> 
+      <value name="NOTE"> 
+        <shadow type="note"> 
+          <field name="NOTE">60</field> 
+        </shadow> 
+      </value> 
+      <value name="BEATS"> 
+        <shadow type="math_number"> 
+          <field name="NUM">0.25</field> 
+        </shadow> 
+      </value> 
+    </block> 
+  `
+})
+
+const musicDefaultBlocks = [
+  'play_drum',
+  'play_note'
+]
+
+const music = prepareBlocks(
+  musicDefaultBlocks,
+  musicBlockLib,
+  (_isStage, blockXml) => `
+    <category name="Musik" id="music">
+        ${blockXml}
+    </category>
+  `
+)
+
 const variables = function () {
   return `
     <category
@@ -1106,6 +1152,7 @@ const categoryMap = {
   sensing,
   operators,
   variables,
+  music,
 }
 
 const xmlOpen = '<xml style="display: none">'
