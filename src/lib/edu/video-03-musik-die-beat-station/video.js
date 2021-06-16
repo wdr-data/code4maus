865883d619968b42e1a09f445147f99463bdffe2
p3nny <patriciaschmitter@googlemail.com>
2021-06-16 13:14:40 +0200

    🚧 Add video_assets - we need to check asset location

--- src/lib/edu/video-03-musik-die-beat-station/video.js
+++ src/lib/edu/video-03-musik-die-beat-station/video.js
@@ -1,3 +1,5 @@
+import title from '../video_assets/Ente-rockt.png'
+
 const url =
   'http://wdradaptiv-vh.akamaihd.net/i/medp/ondemand/weltweit/fsk0/245/2459038/,2459038_35926589,2459038_35926590,2459038_35926591,2459038_35926587,2459038_35926592,2459038_35926588,.mp4.csmil/master.m3u8'
 
@@ -5,6 +7,6 @@ export default {
   id: '03',
   name: 'Die Beat-Station (Musik 3/5)',
   subtitle: 'Jede Figur startet und stoppt per Klick',
-  image: null,
+  image: title,
   video: url,
 }
