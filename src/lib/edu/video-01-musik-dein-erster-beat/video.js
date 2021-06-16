3dc4730738d6d522bb524b819cea8f219d8cdb25
p3nny <patriciaschmitter@googlemail.com>
2021-06-16 13:14:40 +0200

    🚧 Add video_assets - we need to check asset location

--- src/lib/edu/video-01-musik-dein-erster-beat/video.js
+++ src/lib/edu/video-01-musik-dein-erster-beat/video.js
@@ -1,3 +1,5 @@
+import title from '../video_assets/Elefant-rockt.png'
+
 const url =
   'http://wdradaptiv-vh.akamaihd.net/i/medp/ondemand/weltweit/fsk0/245/2459026/,2459026_35926183,2459026_35926184,2459026_35926185,2459026_35926181,2459026_35926186,2459026_35926182,.mp4.csmil/master.m3u8'
 
@@ -5,6 +7,6 @@ export default {
   id: '01',
   name: 'Dein erster Beat (Musik 1/5)',
   subtitle: 'Programmiere mit Blöcken einen Beat',
-  image: null,
+  image: title,
   video: url,
 }
--- src/lib/edu/video-04-musik-stopptanz-maschine/video.js
+++ src/lib/edu/video-04-musik-stopptanz-maschine/video.js
@@ -1,3 +1,5 @@
+import title from '../video_assets/Maus-tanzt.png'
+
 const url =
   'http://wdradaptiv-vh.akamaihd.net/i/medp/ondemand/weltweit/fsk0/245/2459045/,2459045_35926714,2459045_35926715,2459045_35926716,2459045_35926712,2459045_35926717,2459045_35926713,.mp4.csmil/master.m3u8'
 
@@ -5,6 +7,6 @@ export default {
   id: '04',
   name: 'Stopptanz (Musik 4/5)',
   subtitle: 'Die Maus gibt den Takt an beim Stopptanz',
-  image: null,
+  image: title,
   video: url,
 }
