5440f53d82abe6a15e34a276f22f4d3cd6e43629
Camila Espinoza <56554983+cami-espinozaq@users.noreply.github.com>
2023-10-27 13:25:13 +0200

    Update deploy-frontend.sh - original

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -18,6 +18,5 @@ aws s3 sync build s3://${BUCKET} --delete --acl public-read
 for FILE in $(find build -name index.html -printf '%P\n'); do
     aws s3 cp build/${FILE} s3://${BUCKET}/${FILE} --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
 done
-if "$BRANCH" == "production"; then
-    aws s3 cp build/service-worker.js s3://${BUCKET}/service-worker.js --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type application/javascript --acl public-read
-fi
+
+aws s3 cp build/service-worker.js s3://${BUCKET}/service-worker.js --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type application/javascript --acl public-read
