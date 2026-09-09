fc7e638786dfb242fe9fd7af7aa9b19171e912bb
Jannes Höke <mail@jhoeke.de>
2026-09-09 17:11:09 +0200

    fix(dev): suppress error popup during dev
    this one specifically showed up when overing some buttons with a tooltip

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,13 @@ module.exports = {
       },
     },
     historyApiFallback: true,
+    client: {
+      overlay: {
+        // Benign browser notice (triggered e.g. by react-tooltip), not an app error.
+        runtimeErrors: (error) =>
+          !/ResizeObserver loop/.test(error?.message ?? ''),
+      },
+    },
   },
   entry: {
     app: './src/entrypoints/index.jsx',
