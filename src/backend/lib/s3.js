ba6e78c7a81f58117a5786307e5fd5dae4add453
Jannes Höke <mail@jhoeke.de>
2026-09-15 16:10:59 +0200

    Fix storage endpoints for container and browser requests

--- src/backend/lib/s3.js
+++ src/backend/lib/s3.js
@@ -1,44 +1,39 @@
 import AWS from 'aws-sdk'
 
-let options = {
-  params: {
-    Bucket: process.env.STORAGE_BUCKET || process.env.S3_BUCKET_PROJECTS,
-  },
-}
-
-if (
-  'FUNCTIONS_AWS_ACCESS_KEY_ID' in process.env &&
-  'FUNCTIONS_AWS_SECRET_ACCESS_KEY' in process.env
-) {
-  options = {
-    ...options,
-    accessKeyId: process.env.FUNCTIONS_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.FUNCTIONS_AWS_SECRET_ACCESS_KEY,
-  }
-}
-
-if ('FUNCTIONS_AWS_REGION' in process.env) {
-  options = {
-    ...options,
-    region: process.env.FUNCTIONS_AWS_REGION,
-  }
-}
-
-if ('STORAGE_ENDPOINT' in process.env) {
-  options = {
-    ...options,
-    endpoint: process.env.STORAGE_ENDPOINT,
-    s3ForcePathStyle: true,
-    signatureVersion: 'v4',
-  }
-}
-
 export default function (endpoint = null) {
-  if (endpoint !== null) {
+  let options = {
+    params: {
+      Bucket: process.env.STORAGE_BUCKET || process.env.S3_BUCKET_PROJECTS,
+    },
+  }
+
+  if (
+    'FUNCTIONS_AWS_ACCESS_KEY_ID' in process.env &&
+    'FUNCTIONS_AWS_SECRET_ACCESS_KEY' in process.env
+  ) {
     options = {
       ...options,
-      endpoint,
+      accessKeyId: process.env.FUNCTIONS_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.FUNCTIONS_AWS_SECRET_ACCESS_KEY,
     }
   }
+
+  if ('FUNCTIONS_AWS_REGION' in process.env) {
+    options = {
+      ...options,
+      region: process.env.FUNCTIONS_AWS_REGION,
+    }
+  }
+
+  const storageEndpoint = endpoint || process.env.STORAGE_ENDPOINT
+  if (storageEndpoint) {
+    options = {
+      ...options,
+      endpoint: storageEndpoint,
+      s3ForcePathStyle: true,
+      signatureVersion: 'v4',
+    }
+  }
+
   return new AWS.S3(options)
 }
